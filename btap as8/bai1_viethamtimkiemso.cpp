ca1198d507048142d35b09ab7b31beb8d1111f4b
hoa2405 <57317749+hoa2405@users.noreply.github.com>
2019-12-20 15:54:59 +0700

    nop btvn ass8

---
+++ btap as8/bai1_viethamtimkiemso.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+bool timkiemso (int a[],unsigned int x,int n){
+	for (int i=0; i<n; i++){
+	 	if (n==a[i]){
+			return true;
+		}	
+		
+	}
+	return false;
+}
+		
+
+int main (){
+	int ary[5]={1,2,3,4,5};
+	int x;
+	printf ("nhap x= \n");
+	scanf ("%d",&x);
+	if (timkiemso(ary,5,x)){
+	  	printf ("%d co nam trong mang",x);
+	}else{
+	  	printf ("%d ko nam trong mang",x);
+	}
+	return 0;
+}
